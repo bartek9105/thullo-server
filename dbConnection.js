06a5c41ec963885041d482176928a6e193f58386
bartek9105 <bartek9105@gmail.com>
2021-11-10 00:00:07 +0100

    create basic sequelize setup and db connection

---
+++ dbConnection.js
@@ -0,0 +1,16 @@
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  dialect: process.env.DB_DIALECT,
+})
+
+const dbConnection = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+};
+
+module.exports = dbConnection;
--- index.js
+++ index.js
@@ -1,10 +1,10 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const port = 3000;
+const dbConnection = require('./dbConnection');
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+dbConnection();
 
 app.listen(port, () => {
   console.log(`App listening on port: ${port}`);
